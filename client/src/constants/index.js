4b2b20a48141e396a063d5d37178eba8a9f9b449
janzaib-dev <jsyed60009@gmail.com>
2025-12-02 02:05:28 +0500

    chore: create constants folder and setup structure for reusable config

---
+++ client/src/constants/index.js
@@ -0,0 +1,25 @@
+
+export const sideBarLinks = [
+  {
+    icon: "/general/logo.png",
+    route: "/",
+    text: "logo",
+  },  {
+    icon: "/general/home.svg",
+    route: "/menu",
+    text: "menu",
+  },  {
+    icon: "/general/create.svg",
+    route: "/create",
+    text: "create",
+  },  {
+    icon: "/general/updates.svg",
+    route: "/update",
+    text: "update",
+  },  {
+    icon: "/general/messages.svg",
+    route: "/message",
+    text: "message",
+  },
+
+];
