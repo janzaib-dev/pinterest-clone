50975f6be9469ee86be7ed379408a642b330d85c
janzaib-dev <jsyed60009@gmail.com>
2025-12-02 02:06:41 +0500

    feat: add initial pages and project structure for sidebar-based, Gallery and topbar layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
 import './app.css'
+import LeftBar from "./components/LeftBar/LeftBar.jsx";
+import TopBar from "./components/topBar/TopBar.jsx";
+import Gallery from "./components/gallery/Gallery.jsx";
 
 const App = () => {
   return (
-    <div>Hello World</div>
+
+    <div className='app'>
+        <LeftBar />
+        <div className='content'>
+            <TopBar />
+            <Gallery />
+
+        </div>
+    </div>
   )
 }
 
